a8fbba2ada0382e8a91052dd6d65c83b67b5c7d3
Simone-Domenici <domenici.simone00@gmail.com>
2024-10-30 15:47:56 +0100

    ADD ex 7

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,7 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+const findedIndex = students.findIndex(({id}) => id === 2)
+
+console.log(students[findedIndex])
